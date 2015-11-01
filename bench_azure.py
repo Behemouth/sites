cae2bd6281ba8bbf9bfdae9566cea833bb8b522a
Behemouth <Behemouth@users.noreply.github.com>
2015-11-01 18:21:31 +0800

    Add bench failed log

--- bench_azure.py
+++ bench_azure.py
@@ -4,12 +4,14 @@ import time,urllib,re,random
 from termcolor import colored
 
 azure_sites_file = './azure_sites.txt'
+failed_log = './azure_failed.txt'
 
 TEST_URL = 'https://%s.azurewebsites.net/-proxite-/status?r=%s'
 
 def main():
   azure_sites = re.split('\\s+',open(azure_sites_file).read().strip())
   results = []
+  error_sites = []
 
   for domain in azure_sites:
     start = time.time()
@@ -21,8 +23,13 @@ def main():
       color = 'green'
     else:
       color = 'red'
+      error_sites.append(domain)
 
     print colored("%10s:%4ds,Status:%d" % (domain,t,code),color)
+
+  with open(failed_log,'wb') as f:
+    f.write(" ".join(error_sites))
+    f.write("\n")
 
 
 if __name__ == '__main__':
