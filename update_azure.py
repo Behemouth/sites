2a9ca133c70527f092509afdc21df8e84b4f4362
Behemouth <Behemouth@users.noreply.github.com>
2015-11-02 18:11:13 +0800

    Use old azure update

--- update_azure.py
+++ update_azure.py
@@ -25,8 +25,10 @@ def main(args):
   centrice_vip_password = args[3]
 
   dont_update_proxite = False
+  upstream = None
   if len(args) > 4:
     dont_update_proxite = True
+    upstream = args[4]
 
   site_root = path.join(SITES_ROOT,site_id)
 
@@ -51,7 +53,8 @@ def main(args):
 
   def doUpdate(t):
     i,domain = t
-    update(site_root,azure_password,domain,i,dont_update_proxite)
+    update_old(site_root,azure_password,domain,i,upstream)
+    # update(site_root,azure_password,domain,i,dont_update_proxite)
 
   pool = TreadPool(16)
   pool.map(doUpdate, enumerate(azure_sites))
@@ -93,7 +96,83 @@ def update(site_root,password,domain,i,dont_update_proxite):
 
   failed_sites.append(domain)
 
+server_js_tpl = """
+var httpProxy = require('http-proxy'),
+    KeepAliveAgent = require('agentkeepalive');
 
+var proxy = httpProxy.createServer({
+  target: "%s",
+  changeOrigin: true,
+  xfwd: true,
+  agent: new KeepAliveAgent()
+});
+proxy.on('error', function (e) { console.error(e); });
+
+var port = process.env.port || 8000;
+console.info('listening on port', port);
+proxy.listen(port);
+"""
+
+package_json = """
+{
+  "name": "AzureProxy",
+  "version": "0.0.1",
+  "private": true,
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "http-proxy": "^1.9.0",
+    "agentkeepalive": "^2.0.0"
+  }
+}
+"""
+
+web_config = """
+<configuration>
+  <system.webServer>
+    <handlers>
+      <add name="iisnode" path="server.js" verb="*" modules="iisnode" />
+    </handlers>
+    <rewrite>
+      <rules>
+        <rule name="DynamicContent">
+          <action type="Rewrite" url="server.js" />
+        </rule>
+      </rules>
+    </rewrite>
+    <iisnode watchedFiles="*.js;node_modules\\\\*;iisnode.yml" />
+  </system.webServer>
+</configuration>
+"""
+
+def update_old(site_root,password,domain,i,upstream):
+  print("\nUpdating site %d:%s" % (i,domain))
+  server_js = server_js_tpl % upstream
+
+  open(site_root+'/server.js','wb').write(server_js)
+  open(site_root+'/package.json','wb').write(package_json)
+  open(site_root+'/web.config','wb').write(web_config)
+
+  uploadFiles(fileNames = ['server.js','package.json','web.config'],
+              localPath = site_root,
+              server = 'waws-prod-hk1-003.ftp.azurewebsites.windows.net',
+              username =  domain + '\wamcdt',
+              password = password)
+
+
+  for k in xrange(0,AZURE_API_MAX_RETRY):
+    try:
+      cmd = "npm install"
+      azure_exec(domain, password,cmd)
+      print("\nDone site %d:%s" % (i,domain))
+      return
+    except requests.exceptions.RequestException,e:
+      print("Failed %d times, site %d:%s" % (k+1,i,domain))
+      print(e.message)
+      continue
+
+  failed_sites.append(domain)
 
 
 def azure_exec(domain,password,cmd):
@@ -105,6 +184,18 @@ def azure_exec(domain,password,cmd):
     response.raise_for_status()
 
   return True
+
+def uploadFiles(fileNames,localPath,server,username,password):
+  ftp = ftplib.FTP(server, username, password)
+  ftp.cwd('site\\wwwroot')
+  for fname in fileNames:
+    fpath = path.join(localPath,fname)
+    if path.isfile(fpath):
+      fp = open(fpath, 'rb')
+      ftp.storbinary('STOR %s' % fname, fp)
+      fp.close()
+
+  ftp.quit()
 
 def uploadDir(localPath,remotePath,server,username,password):
   ftp = ftplib.FTP(server, username, password)
@@ -138,7 +229,7 @@ When [dont_update_proxite] presents,it will skip WeedProxite update
 
 Example:
   echo "subdomain1 sub2 sub3" > azure_sites.txt
-  ./update-azure.py cdt azure_pass centrice_pass
+  ./update-azure.py cdt azure_passwd centrice_passwd
 
 """)
 
